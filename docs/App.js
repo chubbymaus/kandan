4f4ac14073c6e25084b22e0f56d91ce36c525d54
chubbymaus <court@loftydev.com>
2018-07-13 17:56:30 -0600

    initial commit with basic architecture

---
+++ docs/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { H1 } from 'kandan'
+
+const App = () => {
+  return(
+  <div>
+  <H1>Hello world</H1> 
+  <h1>hello world</h1>
+  </div>
+  )
+}
+
+export default App
